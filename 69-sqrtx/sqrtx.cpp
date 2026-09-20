5e118d376d5f0e44a0209a6eaf382d8693baffa9
Prashant Bhardwaj <prashantbhardwaj77724@gmail.com>
2026-09-21 00:06:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.90%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+    //     int i = 1;
+    //     while(i<=x/i){
+    //         i++;
+    //     }    
+    //     i--;
+    //     return i;
+    // 
+    if (x == 0 || x == 1) return x;
+        
+        int left = 1, right = x, ans = 0;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (mid <= x / mid) {
+                ans = mid;       
+                left = mid + 1;  
+            } else {
+                right = mid - 1; 
+            }
+        }
+        return ans;
+    }
+};
